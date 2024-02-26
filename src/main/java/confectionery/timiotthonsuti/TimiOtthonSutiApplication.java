2f6745270afdcfe033c5d06e5aedd30181bcb44e
VeghLaszlo <veghlas@gmail.com>
2024-02-26 16:43:14 +0100

    Initialization done

---
+++ src/main/java/confectionery/timiotthonsuti/TimiOtthonSutiApplication.java
@@ -0,0 +1,13 @@
+package confectionery.timiotthonsuti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimiOtthonSutiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimiOtthonSutiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/confectionery/timiotthonsuti/TimiOtthonSutiApplicationTests.java
@@ -0,0 +1,13 @@
+package confectionery.timiotthonsuti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimiOtthonSutiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
